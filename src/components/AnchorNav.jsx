6df820cb8cebcaf0aa8f494a1efa64b062197a97
SowmithSripadi <sowmithsripadi1@gmail.com>
2025-05-18 10:54:53 -0400

    changed max width in anchornav

--- src/components/AnchorNav.jsx
+++ src/components/AnchorNav.jsx
@@ -8,7 +8,7 @@ const links = [
 export default function AnchorNav() {
   return (
     <nav
-      className="md:max-w-xl max-w-sm mx-auto mt-14 md:mt-32 flex flex-wrap gap-x-6 gap-y-2 text-sm md:text-base justify-center border-b-2 border-fiuCyan py-3"
+      className="md:max-w-xl max-w-xs mx-auto mt-14 md:mt-32 flex flex-wrap gap-x-6 gap-y-2 justify-center border-b-2 border-fiuCyan py-3"
       aria-label="On this page"
     >
       <span className="font-bold text-16px-sm">On this page:</span>
